9edf7883d3d2fe3406be83e51ab5c497ca4e64b0
dudahl <50145287+dudahl@users.noreply.github.com>
2021-01-07 23:25:11 -0300

    Adicionando Trabalho 1 de Implementação

---
+++ trab1/bubblesort.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>#include <pthread.h>
+#include "timer.h"
+
+void *Bubble(void *arg) {
+	const int tamanho = 100000;
+	int vetor[tamanho];
+	int cont, aux;
+	bool troca;
+	srand(1);
+
+	//populando o vetor com números aleatorios
+	for(cont=0; cont <= tamanho; cont++) {
+		vetor[cont]=(rand() % tamanho);
+	}
+
+	//ordenando-o por bubblesort:
+	do {
+		troca = false;
+		for(cont=0; cont<= tamanho; cont++) {
+			if(vetor[cont] > vetor[cont + 1]) {
+				aux = vetor[cont + 1];
+				vetor[cont + 1] = vetor[cont];
+				vetor[cont] = aux;
+				troca = true;
+			}
+		}
+	}
+	while(troca == true);
+
+	//liberacao dos recursos que a thread esta a consumir.
+	pthread_exit(NULL);
+}
+
+int main() {
+	int numero = 4;
+	double Tinicial, Tfinal, Texec;
+	pthread_t th[numero];
+	int arg, r;
+	
+	//registra tempo inicial
+	GET_TIME(Tinicial);
+
+	for (r=0; r<numero; r++) {
+		//threads sao criadas
+		pthread_create(&th[r],NULL,*Bubble,(void *) &arg);
+	}
+	for (r=0; r<numero; r++) {
+		//faz a thread que chamou(main) esperar ate a thread especificada terminar (nesse caso, como se trata de for, espera-se que todas terminem)
+		pthread_join(th[r],NULL);
+	}
+	
+	//regista tempo final
+	GET_TIME(Tfinal);
+	Texec = Tfinal - Tinicial;
+	
+	printf("Tempo gasto: %.15lf \n", Texec);
+	
+	return 0;
+}
